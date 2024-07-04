c7a1b0a3e13c0721359d5cf0bca4422ad6610858
Abhishek Gaikwad <122622440+Abhishekgaikwad01@users.noreply.github.com>
2024-07-04 16:39:45 +0530

    Update week5.sql

--- week5.sql
+++ week5.sql
@@ -1,1 +1,6 @@
+-- A college needs to develop a system that tracks the Open Elective Subjects of their students. During the start of
+-- the year a lot of students change their subjects and college wants to preserve that entire timeline of the data.
+-- For example, Mohit had selected “Eco philosophy” as his Open Elective subject but later he wished to switch to
+-- “Basics of Accounting” in this scenario the college wants the visibility of Mohit’s previous choice as well instead
+-- of just the active one:
 
